9bd170c3341d86730a8110ab5c82f95b51dd7b83
JorgeDS <jrivera@piensads.com>
2025-03-22 14:18:58 -0600

    SIOMA | frontend | Project configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
